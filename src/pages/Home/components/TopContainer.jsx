d9f5506b2475d942ca2e08803f1e8db2f67b296f
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-09-26 22:11:21 -0300

    Page LogIn

--- src/pages/Home/components/TopContainer.jsx
+++ src/pages/Home/components/TopContainer.jsx
@@ -13,8 +13,8 @@ function TopContainer(){
                         Bem-vindo à nossa plataforma web, desenvolvida para oferecer suporte integral aos agricultores familiares. Nosso foco está na eficiente administração de terras, plantações e na gestão econômica das propriedades. Buscamos aprimorar seu desempenho rural através da implementação da Agricultura de Precisão, garantindo uma administração otimizada e resultados mais sólidos para o seu negócio agrícola.	
                     </p>
                     <div className={styles.info_buttons}>
-                        <a href="../agricultor/formularioagricultor.php"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Cadastro</button></a>
-                        <a href="../login.php"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Login</button></a>
+                        <a href="../../SignUp/"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Cadastro</button></a>
+                        <a href="../../LogIn/"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Login</button></a>
                     </div>
                 </div>
                 <div className={styles.nft_box}>
