0d0aa2dde835c02197cedee184ee9f63480551ed
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-02 19:21:02 -0300

    include favIcon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './routes';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
