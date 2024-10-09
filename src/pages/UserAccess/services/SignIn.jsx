7bef772b51b5e9d482fdd429716a4baab66fccfb
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-09 18:14:46 -0300

    logIn and signUp services

---
+++ src/pages/UserAccess/services/SignIn.jsx
@@ -0,0 +1,13 @@
+function SignIn(){
+    return(
+        <>    
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forget Your Password?</a>
+            <button>Sign In</button>
+        </>
+    )
+}
+
+export default SignIn;
---
+++ src/pages/UserAccess/services/SignUp.jsx
@@ -0,0 +1,14 @@
+function SignUp(){
+    
+    return(
+        <>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+        </>
+    )
+}
+
+export default SignUp
