0b28784ead089f4f1ea794746e4719b2f5d3510a
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-09 18:15:42 -0300

    Update TopContainer.jsx

--- src/pages/Home/components/TopContainer.jsx
+++ src/pages/Home/components/TopContainer.jsx
@@ -1,28 +1,32 @@
-import styles from './TopContainer.module.css'
+import styles from './TopContainer.module.css';
+import { useNavigate } from 'react-router-dom';
 
-function TopContainer(){
-    return(
+function TopContainer() {
+    const navigate = useNavigate();
+
+    const handleButtonClick = (isLogin) => {
+        navigate('/UserAccess', { state: { isLogin } });
+    };
+
+    return (
         <>
             <div className={styles.topContainer}>
                 <div className={styles.info_box}>
-                    <p className={styles.header}>
-                        Bem vindo ao AgrIFFar!
-                    </p>
+                    <p className={styles.header}>Bem vindo ao AgrIFFar!</p>
                     <p className={styles.info_text}>
-
-                        Bem-vindo à nossa plataforma web, desenvolvida para oferecer suporte integral aos agricultores familiares. Nosso foco está na eficiente administração de terras, plantações e na gestão econômica das propriedades. Buscamos aprimorar seu desempenho rural através da implementação da Agricultura de Precisão, garantindo uma administração otimizada e resultados mais sólidos para o seu negócio agrícola.	
+                    Bem-vindo à nossa plataforma web, desenvolvida para oferecer suporte integral aos agricultores familiares. Nosso foco está na eficiente administração de terras, plantações e na gestão econômica das propriedades. Buscamos aprimorar seu desempenho rural através da implementação da Agricultura de Precisão, garantindo uma administração otimizada e resultados mais sólidos para o seu negócio agrícola.
                     </p>
                     <div className={styles.info_buttons}>
-                        <a href="../../SignUp/"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Cadastro</button></a>
-                        <a href="../../LogIn/"> <button className={`${styles.info_btn} ${styles.nav_btn}`}>Login</button></a>
+                        <button className={`${styles.info_btn} ${styles.nav_btn}`} onClick={() => handleButtonClick(true)}>Cadastro</button>
+                        <button className={`${styles.info_btn} ${styles.nav_btn}`} onClick={() => handleButtonClick(false)}>Login</button>
                     </div>
                 </div>
                 <div className={styles.nft_box}>
-                    <img src="/img/LogoAgriffar.jpg" className={styles.nft_pic}/>
+                    <img src="/img/LogoAgriffar.jpg" className={styles.nft_pic} />
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default TopContainer
+export default TopContainer;
