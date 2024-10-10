d871dc3c4f9ef77fe9a6bd5ec3bc757ab48b0c45
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-10 20:15:35 -0300

    Aside and Dashboard page

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import UserAccess from "./pages/UserAccess/index.jsx";
+import Dashboard from './pages/Dashboard/index.jsx';
 
 function MainRoutes(){
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/useraccess" element={<UserAccess />} />
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     );
 }
