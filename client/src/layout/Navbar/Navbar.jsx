90e225771f66e3abb03dbdffa8ecaea58556ff50
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-11 16:50:26 -0300

    Navbar fixed

--- client/src/layout/Navbar/Navbar.jsx
+++ client/src/layout/Navbar/Navbar.jsx
@@ -1,24 +1,8 @@
-import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 import { useNavigate } from 'react-router-dom';
+import Watermark from '../Watermark/Watermark';
 
 function Navbar() {
-    const [showImage, setShowImage] = useState(false); // Estado para controlar a visibilidade da imagem
-    const [exitAnimation, setExitAnimation] = useState(false); // Estado para a animação de saída
-
-    const handleMouseEnter = () => {
-        setExitAnimation(false); // Remove a animação de saída ao entrar
-        setShowImage(true); // Mostra a imagem
-    };
-
-    const handleMouseLeave = () => {
-        setExitAnimation(true); // Ativa a animação de saída
-        // Esconde a imagem após a animação de saída
-        setTimeout(() => {
-            setShowImage(false);
-        }, 400); // O tempo deve ser o mesmo que o da animação de saída
-    };
-
     const navigate = useNavigate();
 
     const handleButtonClick = (isLogin) => {
@@ -27,23 +11,7 @@ function Navbar() {
 
     return (
         <div className={styles.navbar}>
-            <a 
-                href="index.php" 
-                className={styles.logo} 
-                onMouseEnter={handleMouseEnter} // Mostra a imagem ao passar o mouse
-                onMouseLeave={handleMouseLeave} // Oculta a imagem ao tirar o mouse
-            >
-                Agriffar
-            </a>
-
-            {/* A imagem só será renderizada se showImage for true */}
-            {showImage && (
-                <img 
-                    className={`${styles.root} ${exitAnimation ? styles.exit : ''}`} 
-                    src="/img/plantaLogo.png" 
-                    alt="Planta Logo" 
-                />
-            )}
+            <Watermark />
 
             <div className={styles.nav_buttons} id="navMenu">
                 <button className={`${styles.info_btn} ${styles.nav_btn}`} onClick={() => handleButtonClick(true)}>CADASTRO</button>
