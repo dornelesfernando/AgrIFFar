4b09989a650d96c7787f395f599ba571a271a52c
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-26 20:08:55 -0300

    Dashboard Admin page

---
+++ client/src/pages/Dashboard/Admin/index.jsx
@@ -0,0 +1,16 @@
+import styles from './DashboardAdmin.module.css';
+import RightSection from '../layout/RightSection';
+
+function DashboardAdmin(){
+    return(
+        <>
+            <main>
+              
+              
+            </main>
+            <RightSection />
+        </>
+    )
+}
+
+export default DashboardAdmin;
