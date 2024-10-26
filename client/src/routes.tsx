4773151cec21297ba60e0f4afdd5f8e663fbc6d0
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-26 19:07:22 -0300

    Routes Home subpages

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,14 +3,28 @@ import Home from "./pages/Home";
 import UserAccess from "./pages/UserAccess/index.jsx";
 import Dashboard from './pages/Dashboard/index.jsx';
 import NotFound from './pages/NotFound/index.jsx';
+import Settings from './pages/Dashboard/components/Settings.jsx';
+
+import AboutUs from './pages/Home/pages/AboutUs.jsx';
+import OurServices from './pages/Home/Pages/ourServices.jsx';
+import PrivacyPolicy from './pages/Home/Pages/privacyPolicy.jsx';
+import Institution from './pages/Home/Pages/institution.jsx';
+import FAQ from './pages/Home/Pages/FAQ.jsx';
 
 function MainRoutes(){
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/useraccess" element={<UserAccess />} />
+            <Route path="/userAccess" element={<UserAccess />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/notFound" element={<NotFound />} />
+
+            {/* Home page */}
+            <Route path="/home/aboutUs" element={<AboutUs />} />
+            <Route path="/home/ourServices" element={<OurServices />} />
+            <Route path="/home/privacyPolicy" element={<PrivacyPolicy />} />
+            <Route path="/home/institution" element={<Institution />} />
+            <Route path="/home/FAQ" element={<FAQ />} />
         </Routes>
     );
 }
