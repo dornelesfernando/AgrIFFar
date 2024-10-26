808cd51ce7ac0b45270524ebabc1bd02d3eefe96
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-26 19:37:45 -0300

    Dashboard common page routes

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,13 +3,15 @@ import Home from "./pages/Home";
 import UserAccess from "./pages/UserAccess/index.jsx";
 import Dashboard from './pages/Dashboard/index.jsx';
 import NotFound from './pages/NotFound/index.jsx';
-import Settings from './pages/Dashboard/components/Settings.jsx';
 
 import AboutUs from './pages/Home/pages/AboutUs.jsx';
 import OurServices from './pages/Home/Pages/ourServices.jsx';
 import PrivacyPolicy from './pages/Home/Pages/privacyPolicy.jsx';
 import Institution from './pages/Home/Pages/institution.jsx';
 import FAQ from './pages/Home/Pages/FAQ.jsx';
+
+import Settings from './pages/Dashboard/components/Settings.jsx';
+import Profile from './pages/Dashboard/components/Profile.jsx';
 
 function MainRoutes(){
     return (
@@ -25,6 +27,10 @@ function MainRoutes(){
             <Route path="/home/privacyPolicy" element={<PrivacyPolicy />} />
             <Route path="/home/institution" element={<Institution />} />
             <Route path="/home/FAQ" element={<FAQ />} />
+
+            {/* Dashboard commom pages */}
+            <Route path="/dashboard/profile" element={<Profile />} />
+            <Route path="/dashboard/settings" element={<Settings />} />
         </Routes>
     );
 }
