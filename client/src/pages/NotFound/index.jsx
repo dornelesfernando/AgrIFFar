5121de3abd71c361334c32fddf8513ef06de8680
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-26 19:05:40 -0300

    Redirect If Empty and NotFound update

--- client/src/pages/NotFound/index.jsx
+++ client/src/pages/NotFound/index.jsx
@@ -1,12 +1,22 @@
 import styles from './NotFound.module.css';
 
 function NotFound() {
+  function handleButtomClick() {
+    window.history.back();
+    window.history.back();
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.text}>
         <h2>404 - Página não encontrada</h2>
         <h1>Essa Página Está em Construção!</h1>
         <h2>Em breve estará disponível</h2>
+        <a onClick={() => handleButtomClick()}>
+          <div className={styles.returnPage}>
+            <h2>Retornar para a página anterior?</h2>
+          </div>
+        </a>
       </div>
     </div>
   );
--- client/src/service/RedirectIfEmpty.jsx
+++ client/src/service/RedirectIfEmpty.jsx
@@ -1,15 +1,15 @@
+// RedirectIfEmpty.js
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const RedirectIfEmpty = ({ children }) => {
-    // Verifica se o conteúdo está vazio
     const isEmpty = React.Children.count(children) === 0;
 
-    // Se estiver vazio, redireciona para a página de não encontrado
     if (isEmpty) {
         return <Navigate to="/notfound" />;
     }
 
-    return <>{children}</>; // Retorna os filhos se não estiver vazio
+    return <>{children}</>;
 };
 
 export default RedirectIfEmpty;
