07e706a54159749aed962f714e1259205585f770
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-10-26 19:36:50 -0300

    Dashboard commom pages

---
+++ client/src/pages/Dashboard/components/Profile.jsx
@@ -0,0 +1,12 @@
+import RedirectIfEmpty from '../../../service/RedirectIfEmpty';
+import styles from './Profile.module.css'
+
+function Profile(){
+    return(
+        <>
+            <RedirectIfEmpty />
+        </>
+    )
+}
+
+export default Profile;
---
+++ client/src/pages/Dashboard/components/Settings.jsx
@@ -0,0 +1,12 @@
+import RedirectIfEmpty from '../../../service/RedirectIfEmpty';
+import styles from './Settings.module.css'
+
+function Settings(){
+    return(
+        <>
+            <RedirectIfEmpty />
+        </>
+    )
+}
+
+export default Settings;
