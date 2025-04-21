3ceddcdd281c66835c39b6130ac8a9a9b9dbe640
Fernando Dorneles <fernandodorneles95@gmail.com>
2025-04-21 09:05:42 -0300

    update 21042025

---
+++ client/src/pages/Dashboard/Admin/components/NewUsers.jsx
@@ -0,0 +1,35 @@
+import styles from './NewUsers.module.css'
+
+function NewUsers(){
+    return(
+        <>
+            <div className={styles.newUsers}>
+                <h2>New Users</h2>
+                <div className={styles.userList}>
+                    <div className={styles.user}>
+                        <img src="" />
+                        <h2>Jack</h2>
+                        <p>54 Min Ago</p>
+                    </div>
+                    <div className={styles.user}>
+                        <img src="images/profile-3.jpg" />
+                        <h2>Amir</h2>
+                        <p>3 Hours Ago</p>
+                    </div>
+                    <div className={styles.user}>
+                        <img src="images/profile-4.jpg" />
+                        <h2>Ember</h2>
+                        <p>6 Hours Ago</p>
+                    </div>
+                    <div className={styles.user}>
+                        <img src="images/plus.png" />
+                        <h2>More</h2>
+                        <p>New User</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NewUsers;
